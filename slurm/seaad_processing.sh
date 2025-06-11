d4ddc999232607c99fd10ab9686c3571547d6ac5
DarthTimothee <timoverlaan@gmail.com>
2025-06-11 18:36:34 +0200

    Implement basic scanpy pipeline and script for processing seaAD

---
+++ slurm/seaad_processing.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+#SBATCH --account=ewi-insy-prb
+#SBATCH --partition=insy,general # Request partition. Default is 'general' 
+#SBATCH --qos=short         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
+#SBATCH --time=1:00:00      # Request run time (wall-clock). Default is 1 minute
+#SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
+#SBATCH --cpus-per-task=16   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
+#SBATCH --mem=500GB          # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
+#SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
+#SBATCH --output=slurm/out/%j_all.out # Set name of output log. %j is the Slurm jobId
+#SBATCH --error=slurm/out/%j_all.out # Set name of error log. %j is the Slurm jobId
+/usr/bin/scontrol show job -d "$SLURM_JOB_ID"  # check sbatch directives are working
+
+apptainer exec --writable-tmpfs --pwd /opt/app --containall \
+	--bind src/:/opt/app/src/ \
+	--bind data/:/opt/app/data/ \
+	--bind out/:/opt/app/out/ \
+	./container_pixi_0-1-1.sif pixi run python -u src/scanpy_pipeline.py \
+	data/seaAD/PFC/RNAseq/SEAAD_A9_RNAseq_final-nuclei.2024-02-13.h5ad \  # input file
+	data/seaAD/PFC/RNAseq/seaad1000_k30.h5ad \  # output file
+	--layer UMIs \
+	--n_genes 1000 \  # number of highly variable genes to select
+	--k-neighbors 30
---
+++ src/scanpy_pipeline.py
@@ -0,0 +1,106 @@
+import scanpy as sc
+import anndata as ad
+import argparse 
+
+from tqdm import tqdm
+
+
+parser = argparse.ArgumentParser(description="Performs the basic scanpy processing steps on a given h5ad file.")
+parser.add_argument("path", type=str, help="Path to the h5ad file to process.")
+parser.add_argument("output", type=str, help="Path to save the processed h5ad file.")
+parser.add_argument("--layer", type=str, default=None, help="Layer to select for processing. If None, the main data will be used.")
+parser.add_argument("--raw", action="store_true", help="If set, the raw data will be used instead of the main data.")
+parser.add_argument("--n-genes", type=int, default=2000, help="Number of highly variable genes to select. Default is 2000.")
+parser.add_argument("--k-neighbors", type=int, default=30, help="Number of neighbors for the KNN graph. Default is 30.")
+parser.add_argument("--individual-pca", action="store_true", help="If set, PCA for the KNN graph will be computed for each individual separately.")
+
+# TODO: add option to use existing gene list from another h5ad, or csv
+
+args = parser.parse_args()
+
+
+if __name__ == "__main__":
+
+    print("Starting scanpy processing pipeline...")
+    print("Arguments:")
+    print(f"  Path: {args.path}")
+    print(f"  Output: {args.output}")
+    print(f"  Layer: {args.layer}")
+    print(f"  Use raw data: {args.raw}")
+    print(f"  Number of highly variable genes: {args.n_genes}")
+    print(f"  Number of neighbors for KNN graph: {args.k_neighbors}")
+    print(f"  Individual PCA: {args.individual_pca}")
+    print("\n")
+
+    print("Importing h5ad file: ", args.path)
+    adata = ad.read_h5ad(args.path)
+
+    print(adata)
+    print()
+
+    if args.raw:
+        print("Using raw data.")
+        if adata.raw is not None:
+            # TODO: this is not tested yet, but it should work
+            adata.X = adata.raw.X.copy()
+            adata.var_names = adata.raw.var_names
+            adata.obs_names = adata.raw.obs_names
+        else:
+            raise ValueError("Raw data is not available in the AnnData object.")
+
+    if args.layer is not None:
+        print(f"Using layer: {args.layer}")
+        if args.layer in adata.layers:
+            del adata.X
+            adata.X = adata.layers[args.layer].copy()
+            del adata.layers[args.layer]
+            del adata.layers
+        else:
+            raise ValueError(f"Layer '{args.layer}' not found in the AnnData object.")
+    
+    print("Processing data...")
+    
+    print(f"Filtering cells and genes with minimum thresholds. Shape before filtering: {adata.shape}")
+    sc.pp.filter_cells(adata, min_genes=200)
+    sc.pp.filter_genes(adata, min_cells=5)
+    print(f"Shape after filtering: {adata.shape}")
+
+    print(f"Selecting highly variable genes...")
+    sc.pp.highly_variable_genes(adata, flavor='seurat_v3', n_top_genes=args.n_genes, subset=True)
+    print(f"Number of highly variable genes selected: {adata.var['highly_variable'].sum()}")
+
+    print("Normalizing and log-transforming data...")
+    sc.pp.normalize_total(adata, target_sum=1e6)  # Normalize to 1 million reads per cell
+    sc.pp.log1p(adata)
+    print("Done.")
+
+    if not args.individual_pca:
+        print("Calculating PCA...")
+        sc.pp.pca(adata, n_comps=50)
+        print("Done.")
+    else:
+        print("Skipping full PCA, will compute PCA for each donor separately.")
+
+    print("Building KNN graph...")
+    slices = []
+    for donor in tqdm(adata.obs["Donor ID"].unique(), desc="Building donor KNN graphs"):
+        donor_slice = adata[adata.obs["Donor ID"] == donor].copy()
+        if args.individual_pca:  # if individual PCA is requested
+            sc.pp.pca(donor_slice, n_comps=50)
+        sc.pp.neighbors(donor_slice, n_neighbors=args.k_neighbors, use_rep="X_pca")
+        slices.append(donor_slice)
+
+    del adata
+    adata = ad.concat(
+        slices,
+        merge="same",
+        uns_merge="same",
+        label="all",
+        index_unique="-",
+        pairwise=True,
+    )
+
+    # Save the processed data
+    print(f"\nSaving processed data to: {args.output}")
+    adata.write_h5ad(args.output, compression="gzip")
+    print("Processing complete.\n\n")
