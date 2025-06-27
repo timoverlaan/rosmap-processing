c132c74083dadc624ad243753a731bb2636c2f57
DarthTimothee <timoverlaan@gmail.com>
2025-06-27 21:14:26 +0200

    Rosmap top1k genes script

---
+++ slurm/rosmap_top1k_genes.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+#SBATCH --account=ewi-insy-prb
+#SBATCH --partition=insy,general # Request partition. Default is 'general' 
+#SBATCH --qos=long         # Request Quality of Service. Default is 'short' (maximum run time: 4 hours)
+#SBATCH --time=12:00:00      # Request run time (wall-clock). Default is 1 minute
+#SBATCH --ntasks=1          # Request number of parallel tasks per job. Default is 1
+#SBATCH --cpus-per-task=4   # Request number of CPUs (threads) per task. Default is 1 (note: CPUs are always allocated to jobs per 2).
+#SBATCH --mem=400GB          # Request memory (MB) per node. Default is 1024MB (1GB). For multiple tasks, specify --mem-per-cpu instead
+#SBATCH --mail-type=END     # Set mail type to 'END' to receive a mail when the job finishes. 
+#SBATCH --output=slurm/out/%j_all.out # Set name of output log. %j is the Slurm jobId
+#SBATCH --error=slurm/out/%j_all.out # Set name of error log. %j is the Slurm jobId
+/usr/bin/scontrol show job -d "$SLURM_JOB_ID"  # check sbatch directives are working
+
+apptainer exec --writable-tmpfs --pwd /opt/app --containall \
+	--bind src/:/opt/app/src/ \
+	--bind data/:/opt/app/data/ \
+	--bind out/:/opt/app/out/ \
+	./container_pixi_0-1-2.sif pixi run python -u src/scanpy_pipeline.py \
+		data/raw/ROSMAP_MIT/combined.h5ad \
+		data/processed/rosmap_mit_Mathysgenes_k30.h5ad \
+		--n-genes 1000 --k-neighbors 30
